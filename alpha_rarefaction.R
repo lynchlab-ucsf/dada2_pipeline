748597e4660e7375dbbc9972fa259af7c0bc3d29
lynchlab-ucsf <lynchlabequipment@gmail.com>
2021-03-15 15:55:39 -0700

    Update to Alpha Rarefaction Script using 2thiles instead

--- alpha_rarefaction.R
+++ alpha_rarefaction.R
@@ -1,13 +1,21 @@
 library(vegan)
 library(phyloseq)
+library(dplyr)
 
 args <- commandArgs(TRUE)
 
 phy <- readRDS(as.character(args[1]))
 
+set.max <- as.numeric(args[2])
+
+uselevels <- data.frame(read.depth=sample_sums(phy)) %>%
+  mutate(groups=factor(ntile(read.depth, 25))) %>% 
+  group_by(groups) %>%
+  summarize(min(read.depth))
+
 depth <- samples.kept <- c()
-for(i in 1:25) {
-  depth[i] <- floor(max(sample_sums(phy))/25)*i
+for(i in 1:nrow(uselevels[,2])) {
+  depth[i] <- uselevels[i,2]
   samples.kept[i] <- sum(sample_sums(phy) > depth[i])
 }
 
